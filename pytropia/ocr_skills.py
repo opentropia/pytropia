3565d44d53756a361c2c6f347e49876a83ed4af1
Anton Danielsson <danielsson.anton@gmail.com>
2021-05-23 21:06:34 +0200

    Make page number scanning more robust

--- pytropia/ocr_skills.py
+++ pytropia/ocr_skills.py
@@ -72,10 +72,33 @@ def margin(img, margin_x, margin_y):
     return crop(img, (margin_x, margin_y), (w - margin_x, h - margin_y))
 
 def ocr_page_number(img):
-    # psm 7 = "Treat the image as a single text line."
+    # psm 8 = Treat the image as a single word.
+
+    # Parameters for image pre-processing
+    scale_percent = 400
+    threshold = 130
+
+    # Upscale
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    img = cv2.resize(img, dim, interpolation = cv2.INTER_CUBIC)
+
+    # Convert to gray scale
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Threshold
+    _, img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+
     chars = '0123456789/'
     result = pytesseract.image_to_string(img, lang='eng', config=f'-c tessedit_char_whitelist="{chars}" --psm 7')
-    return result.partition('\n')[0].split('/')
+
+    result = result.partition('\n')[0].split('/')
+
+    if len(result) != 2:
+        eprint("Warning: Failed to parse page number, defaulting to 1/1")
+        return (1, 1)
+
+    return result
 
 def ocr_total_skills(img):
     # psm 8 = Treat the image as a single word.
