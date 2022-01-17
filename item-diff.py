48e31a3858b456f4f3f26e6faa4284d94c34e2ec
Anton Danielsson <danielsson.anton@gmail.com>
2022-01-17 22:21:45 +0100

    Add markup calculation to item-diff
    Also makes the printing prettier by using 'tabulate'

--- item-diff.py
+++ item-diff.py
@@ -5,6 +5,8 @@ import json
 import re
 import sys
 
+from tabulate import tabulate
+
 # The json format from
 #  https://account.entropiauniverse.com/account/my-account/my-items/json.xml
 # looks like this:
@@ -96,6 +98,8 @@ def main():
                         help='End items json', required=True)
     parser.add_argument('--container-filter', '-f',
                         default="carried", help='Filter based on container type')
+    parser.add_argument('--mark-up', '-m', default=None, type=argparse.FileType('r'),
+                        help='File with markup values. If this is provided MU in/out will also be calculated')
 
     args = parser.parse_args()
 
@@ -104,13 +108,21 @@ def main():
     item_values_end = get_item_values(
         args.file_end.name, args.container_filter)
 
+    mu_data = {}
+    if args.mark_up:
+        with open(args.mark_up.name) as mu_json_file:
+            mu_data = json.load(mu_json_file)
+
+    total_mu_pos = 0
+    total_mu_neg = 0
+
     # Diff ped value for each item type
     all_keys = set(item_values_start.keys() | item_values_end.keys())
     cost = 0.000000001
     ret = 0.000000001
     item_diff = {}
     for key in all_keys:
-        diff_ped = item_values_end.get(
+        diff_ped = item_values_end.get( 
             key, (0, 0))[0] - item_values_start.get(key, (0, 0))[0]
         diff_quantity = item_values_end.get(
             key, (0, 0))[1] - item_values_start.get(key, (0, 0))[1]
@@ -121,18 +133,50 @@ def main():
             ret += diff_ped
 
         if abs(diff_ped) > sys.float_info.epsilon or abs(diff_quantity) > 0:
-            item_diff[key] = (diff_ped, int(diff_quantity))
+            markup = 0
+            if key in mu_data:
+                if "percent" in mu_data[key]:
+                    markup = diff_ped * (mu_data[key]["percent"] / 100 - 1)
+                elif "ped" in mu_data[key]:
+                    markup = diff_quantity * mu_data[key]["ped"]
+                elif "other-percent" in mu_data[key]:
+                    markup = diff_ped * (mu_data[mu_data[key]["other-percent"]]["percent"] / 100 - 1)
+
+            if diff_quantity < 0 or diff_ped < 0:
+                total_mu_neg += abs(markup)
+            else:
+                total_mu_pos += abs(markup)
+
+            item_diff[key] = [diff_ped, int(diff_quantity), markup]
+
+    mu_delta = total_mu_pos - total_mu_neg
 
     if len(item_diff) == 0:
         print("No differences found!")
         return 0
 
+    results_expense = []
+    results_gained = []
     # Print diff sorted on ped value
     for key, item in sorted(item_diff.items(), key=lambda x: x[1][0], reverse=True):
-        print(f"{key}: {item[0]:.2f} PED ({item[1]})")
+        if item[1] > 0:
+            results_gained.append([key] + item)
+        else:
+            results_expense.append([key] + item)
 
-    print(f"\nCost, Return, Delta")
-    print(f"{cost:.2f}  {ret:.2f}  {ret - cost:.2f}  {(ret / cost) * 100:.2f}%")
+    results_expense.append(["[Sum]", cost, 0, total_mu_neg])
+    results_gained.append(["[Sum]", ret, 0, total_mu_pos])
+
+    print(tabulate(results_expense, headers=['Expenses', 'PED', 'Count', 'Markup'], floatfmt=".2f"))
+    print()
+    print(tabulate(results_gained, headers=['Loot', 'PED', 'Count', 'Markup'], floatfmt=".2f"))
+
+    print('\nMarkup')
+    print(tabulate([[total_mu_pos, total_mu_neg, mu_delta, (mu_delta / cost) *100]], headers=['Gained', 'Lost', 'Delta', 'Delta (%)'], floatfmt=".2f"))
+
+    print('\nSummary')
+    print(tabulate([[cost, ret, ret - cost, (ret / cost) *100, ret - cost + mu_delta, ((ret+mu_delta) / cost) * 100]],
+         headers=['Cost', 'Return', 'Delta', 'Delta (%)', 'Delta MU', 'Delta MU (%)'], floatfmt=".2f"))
 
     # TODO: Group cost based on things like Shrapnel, Ammo, Weapons, Tools, FAP, Armor and so on.
 
