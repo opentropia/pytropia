d013bf715e11543d7e572339383f2678698cbc8f
Anton Danielsson <danielsson.anton@gmail.com>
2022-03-27 12:23:04 +0200

    Update loot simulator to use multipliers that are based on real data
    Also includes shrap bonus and looter/eff

--- loot-sim.py
+++ loot-sim.py
@@ -2,53 +2,48 @@
 
 import numpy as np
 import random
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
+from sklearn import multiclass 
 
-cost_per_mob = .10 # PED
+efficiency = 80
+looter = 50
+
+cost_per_mob = 0.10 # PED
 cost_randomness = 0.10 # The randomness of a kill cost, 0.05 = +-5%
-num_mobs = 120000
+num_mobs = 5000
 
-# First value will be calculated based on the other multipliers to
-# satisfy that the total propability is 1 and long_term_return.
-loot_multipliers   = [0.0, 0.8, 1  , 2   , 10   , 100   , 1000]
-loot_probabilities = [0.0, 0.2, 0.1, 0.05, 0.01, 0.0005, 0.00001]
+bonus_shrap_prop = 0.1
+bonus_shrap_multi = 0.6
+bonus_shrap_spread = 0.05
 
-loot_randomness = 0.1 # The randomness of a loot instance, 0.5 = +-50%
-                      # Applied after the multiplier has been chosen
-
-# This is the long time return. 1 = 100%, 0.95 = 95% and so on.
-long_term_return_tt = 0.95
-long_term_return_mu = 0.05
-long_term_return = long_term_return_tt + long_term_return_mu
+# Tuned for 93% TT from normal loot + 6% from bonus shrap. 99% total
+loot_probabilities  = [0.100, 0.100, 0.250, 0.350, 0.120, 0.040, 0.020, 0.01, 0.0025, 0.0035, 0.002, 0.00189, 0.0001, 0.00001]
+loot_multipliers    = [0.282, 0.376, 0.469, 0.525, 0.941, 1.883, 3.765, 7.53, 11.275, 15.045, 18.87, 38     , 150   , 1500]
+loot_spreads        = [0.028, 0.038, 0.046, 0.099, 0.094, 0.188, 0.375, 0.75,  1.025,  1.495,  2.33, 10     , 50    , 500]
 
 print_loots = False
 plot_outcome = True
 
-if len(loot_multipliers) != len(loot_probabilities):
+if len(loot_multipliers) != len(loot_probabilities) or len(loot_multipliers) != len(loot_spreads):
     print("lengths mismatch")
     exit(1)
 
-# Derive first probability
-first_prop = sum(loot_probabilities)
+# TODO: calculate and print expected long time return based on multis and shrap (with and without looter/eff)
 
-if first_prop >= 1:
-    print("invalid loot_probabilities")
+props = sum(loot_probabilities)
+print(f"prop sum = {props}")
+
+if abs(props - 1) > 0.000001:
+    print("prop must be 1")
     exit(1)
 
-loot_probabilities[0] = 1 - first_prop
-
-# Derive first multiplier
-return_left = long_term_return - sum([a * b for a, b in zip(loot_multipliers, loot_probabilities)])
-loot_multipliers[0] = return_left / loot_probabilities[0]
-
-if loot_multipliers[0] <= 0:
-    print("invalid loot_multipliers")
-    exit(1)
 
 print("Multipliers:")
 print(loot_multipliers)
 print("Probabilities:")
 print(loot_probabilities)
+print("Spreads")
+print(loot_spreads)
 
 loot_bins = np.cumsum(loot_probabilities)
 print("Bins:")
@@ -65,13 +60,23 @@ for kill in range(num_mobs):
     for bin in range(len(loot_bins)):
         if roll <= loot_bins[bin]:
             multiplier = loot_multipliers[bin]
+            spread = loot_spreads[bin]
             break
 
     # Multiplier with randomness
-    multiplier = multiplier * (1 + random.uniform(-loot_randomness, loot_randomness))
+    multiplier = random.uniform(multiplier-spread, multiplier+spread)
     # Cost with randomness
     cost = cost_per_mob * (1 + random.uniform(-cost_randomness, cost_randomness))
     loot = cost * multiplier
+
+    # Roll bonus shrap
+    roll = random.random()
+    if roll < bonus_shrap_prop:
+        loot += cost * random.uniform(bonus_shrap_multi-bonus_shrap_spread, bonus_shrap_multi+bonus_shrap_spread)
+
+    factor = 0.86 + 0.07 * (efficiency/100) + 0.07 * (looter/100)
+    loot = loot * factor
+
     total_cost += cost
     total_loot += loot
 
@@ -83,9 +88,13 @@ for kill in range(num_mobs):
 
 print(f"Total loot {total_loot:.0f}, Total cost {total_cost:.0f}, Delta {total_loot-total_cost:.0f} ({(total_loot/total_cost)*100:.2f}%)")
 
+multis = []
+for i in range(0, len(loots)):
+    multis.append(loots[i]/costs[i])
+
 if plot_outcome:
     fig, axs = plt.subplots(2, 2)
-    fig.suptitle(f"Long time return {long_term_return*100:.2f}%, Cost/Mob {cost_per_mob:.2f}, Num Mobs {num_mobs}, Loot {total_loot:.0f}, Cost {total_cost:.0f}, Delta {total_loot-total_cost:.0f} ({(total_loot/total_cost)*100:.2f}%)")
+    fig.suptitle(f"Eff: {efficiency}%, Looter: {looter} Cost/Mob {cost_per_mob:.2f}, Num Mobs {num_mobs}, Loot {total_loot:.0f}, Cost {total_cost:.0f}, Delta {total_loot-total_cost:.0f} ({(total_loot/total_cost)*100:.2f}%)")
 
     t = np.linspace(0, num_mobs, num_mobs)
     axs[0, 0].set_title("Loot over time")
@@ -95,10 +104,10 @@ if plot_outcome:
     axs[0, 0].grid()
 
     returns = np.cumsum(loots)
-    axs[0, 1].set_title("Accumulated Return")
-    axs[0, 1].set_xlabel("Num kills")
-    axs[0, 1].set_ylabel("Return (PED)")
-    axs[0, 1].plot(t, returns)
+    axs[0, 1].set_title("Sorted Multis")
+    axs[0, 1].set_xlabel("Kills")
+    axs[0, 1].set_ylabel("Loot (Multiplier)")
+    axs[0, 1].plot(np.sort(multis))
     axs[0, 1].grid()
 
     cost = np.cumsum(costs)
