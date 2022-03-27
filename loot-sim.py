ec2b81441859147ed3b1c475c3078a5c1fdc663c
Anton Danielsson <danielsson.anton@gmail.com>
2022-03-27 16:06:03 +0200

    Add more groups to loot-sim

--- loot-sim.py
+++ loot-sim.py
@@ -8,18 +8,18 @@ from sklearn import multiclass
 efficiency = 80
 looter = 50
 
-cost_per_mob = 0.10 # PED
+cost_per_mob = 1.00 # PED
 cost_randomness = 0.10 # The randomness of a kill cost, 0.05 = +-5%
-num_mobs = 5000
+num_mobs = 10000
 
 bonus_shrap_prop = 0.1
 bonus_shrap_multi = 0.6
 bonus_shrap_spread = 0.05
 
 # Tuned for 93% TT from normal loot + 6% from bonus shrap. 99% total
-loot_probabilities  = [0.100, 0.100, 0.250, 0.350, 0.120, 0.040, 0.020, 0.01, 0.0025, 0.0035, 0.002, 0.00189, 0.0001, 0.00001]
-loot_multipliers    = [0.282, 0.376, 0.469, 0.525, 0.941, 1.883, 3.765, 7.53, 11.275, 15.045, 18.87, 38     , 150   , 1500]
-loot_spreads        = [0.028, 0.038, 0.046, 0.099, 0.094, 0.188, 0.375, 0.75,  1.025,  1.495,  2.33, 10     , 50    , 500]
+loot_probabilities  = [0.100, 0.100, 0.250, 0.350, 0.120, 0.040, 0.020, 0.01, 0.0025, 0.0035, 0.002, 0.0018733, 0.00006, 0.00004, 0.00002, 0.0000067]
+loot_multipliers    = [0.282, 0.376, 0.469, 0.525, 0.941, 1.883, 3.765, 7.53, 11.275, 15.045, 18.87, 33.5     , 150    , 250    , 700    , 1500]
+loot_spreads        = [0.028, 0.038, 0.046, 0.099, 0.094, 0.188, 0.375, 0.75,  1.025,  1.495,  2.33, 10       ,  50    , 100    , 200    ,  500]
 
 print_loots = False
 plot_outcome = True
@@ -28,8 +28,6 @@ if len(loot_multipliers) != len(loot_probabilities) or len(loot_multipliers) != 
     print("lengths mismatch")
     exit(1)
 
-# TODO: calculate and print expected long time return based on multis and shrap (with and without looter/eff)
-
 props = sum(loot_probabilities)
 print(f"prop sum = {props}")
 
@@ -37,6 +35,15 @@ if abs(props - 1) > 0.000001:
     print("prop must be 1")
     exit(1)
 
+factor = 0.86 + 0.07 * (efficiency/100) + 0.07 * (looter/100)
+avg_returns = 0
+for bin in range(len(loot_probabilities)):
+    avg_returns += loot_probabilities[bin] * loot_multipliers[bin]
+avg_shrap = bonus_shrap_prop *bonus_shrap_multi
+long_term_total_norm = (avg_shrap+avg_returns)*100
+long_term_total = long_term_total_norm * factor
+print(f"Long term return: {avg_returns*100:.2f}% + Bonus shrap: {avg_shrap*100:.2f}%, Total: {long_term_total_norm:.2f}%")
+print(f"Returns with factor: {factor:.4f} from looter {looter} and eff {efficiency}%: {long_term_total:.2f}%")
 
 print("Multipliers:")
 print(loot_multipliers)
@@ -74,7 +81,6 @@ for kill in range(num_mobs):
     if roll < bonus_shrap_prop:
         loot += cost * random.uniform(bonus_shrap_multi-bonus_shrap_spread, bonus_shrap_multi+bonus_shrap_spread)
 
-    factor = 0.86 + 0.07 * (efficiency/100) + 0.07 * (looter/100)
     loot = loot * factor
 
     total_cost += cost
@@ -94,7 +100,7 @@ for i in range(0, len(loots)):
 
 if plot_outcome:
     fig, axs = plt.subplots(2, 2)
-    fig.suptitle(f"Eff: {efficiency}%, Looter: {looter} Cost/Mob {cost_per_mob:.2f}, Num Mobs {num_mobs}, Loot {total_loot:.0f}, Cost {total_cost:.0f}, Delta {total_loot-total_cost:.0f} ({(total_loot/total_cost)*100:.2f}%)")
+    fig.suptitle(f"Eff: {efficiency}%, Looter: {looter}, Expected ret {long_term_total:.2f}% Cost/Mob {cost_per_mob:.2f}, Num Mobs {num_mobs}, Loot {total_loot:.0f}, Cost {total_cost:.0f}, Delta {total_loot-total_cost:.0f} ({(total_loot/total_cost)*100:.2f}%)")
 
     t = np.linspace(0, num_mobs, num_mobs)
     axs[0, 0].set_title("Loot over time")
